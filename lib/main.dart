95b62a40383d49b49b918030060ad1cd88099e5b
WhalySoft <hamzaqasim.co@gmail.com>
2023-10-29 00:26:34 +0500

    Initial for user guide using tutorial_coach_mark package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+import 'package:untitled1/user_guide_tutorial/user_guide_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,151 +7,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainScreen(),
+      home: const UserGuideScreen(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  final random = Random();
-  final GlobalKey _floatingButtonKey = GlobalKey();
-  final GlobalKey _editButtonKey = GlobalKey();
-  final GlobalKey _settingsButtonKey = GlobalKey();
-
-  @override
-  void initState() {
-    super.initState();
-
-    _createTutorial();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('User guide'),
-        actions: [
-          IconButton(
-            key: _editButtonKey,
-            onPressed: () {
-              // Do nothing
-            },
-            icon: const Icon(
-              Icons.edit,
-            ),
-          ),
-          IconButton(
-            key: _settingsButtonKey,
-            onPressed: () {
-              // Do nothing
-            },
-            icon: const Icon(
-              Icons.settings,
-            ),
-          )
-        ],
-      ),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return const Card(
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  child: FlutterLogo(size: 100),
-                ),
-                title: Text('Hamza Qasim,'),
-                subtitle: Text('with 1+ year of experience.'),
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        key: _floatingButtonKey,
-        onPressed: () {
-          // Do nothing
-        },
-        tooltip: 'Floating action',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  Future<void> _createTutorial() async {
-    final targets = [
-      TargetFocus(
-        identify: 'floatingButton',
-        keyTarget: _floatingButtonKey,
-        alignSkip: Alignment.topLeft,
-        contents: [
-          TargetContent(
-            align: ContentAlign.top,
-            builder: (context, controller) => Text(
-              'Use this button to add new elements to the list',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.copyWith(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-      TargetFocus(
-        identify: 'editButton',
-        keyTarget: _editButtonKey,
-        alignSkip: Alignment.bottomCenter,
-        contents: [
-          TargetContent(
-            align: ContentAlign.bottom,
-            builder: (context, controller) => Text(
-              'You can edit the entries by pressing on the edit button',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.copyWith(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-      TargetFocus(
-        identify: 'settingsButton',
-        keyTarget: _settingsButtonKey,
-        alignSkip: Alignment.bottomCenter,
-        contents: [
-          TargetContent(
-            align: ContentAlign.bottom,
-            builder: (context, controller) => Text(
-              'Configure the app in the settings screen',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge
-                  ?.copyWith(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-    ];
-
-    final tutorial = TutorialCoachMark(
-      targets: targets,
-    );
-
-    Future.delayed(const Duration(milliseconds: 500), () {
-      tutorial.show(context: context);
-    });
-  }
-}
